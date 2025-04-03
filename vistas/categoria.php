0003d8fe0357498109c6b5ce12be6c188a595d69
AlexSRCode1 <suarezemmanuelr@gmail.com>
2025-04-03 10:04:04 -0400

    Primer commit

---
+++ vistas/categoria.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    
+
+</head>
+<body>
+
+    <?php
+    // require_once"referencias.php";
+    include "menu.php";
+    ?>
+    
+    <div class="container">
+    <h1>Bienvenidos a registrar categorias</h1>
+    </div>
+
+    
+
+
+</body>
+</html>
---
+++ vistas/js/script.js
@@ -0,0 +1,4 @@
+import DataTable from 'datatables.net-dt';
+let table = new DataTable('#myTable', {
+    // config options...<font></font>
+});
