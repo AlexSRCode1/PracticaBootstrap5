fc554534b1a1c2c1c4fc41fc379171b780ef7b11
AlexSRCode1 <suarezemmanuelr@gmail.com>
2025-05-15 11:40:48 -0400

    boton modal bootstrap5.0.2

--- vistas/listaProductos.php
+++ vistas/listaProductos.php
@@ -14,8 +14,11 @@
 <body>
  <div class="container">
     <h3> Lista de producto</h3>   
-    <a href="#" class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true"> Agregar Producto</a>
-    
+   <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Agregar Producto
+</button>
+
     <div class="row">
         <table class="table">
 
@@ -84,6 +87,25 @@
  </div>    
 
 
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
